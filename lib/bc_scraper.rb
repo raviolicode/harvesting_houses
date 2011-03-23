24ce7865eec24cfb0a21c724536806adcf0d9310
Lucia Escanellas <raviolicode@gmail.com>
2011-03-23 04:08:19 -0300

    Scraper parent class

--- lib/bc_scraper.rb
+++ lib/bc_scraper.rb
@@ -39,25 +39,5 @@ class BuscandoCasaScraper < Scraper
     #               :ref => ref)
     # :description => house_params['descripcion'],
   end
-
-  def clean_field element 
-    # TODO: regex!
-    # TODO: bad smell! esto no va aca!
-    element.inner_html.gsub(/&nbsp;/, '').downcase
-  end
-
-  def get_document_from_url base_url, params=nil
-    #TODO: tampoco va aca
-    url = URI.escape(@urls[base_url] + params)
-    Nokogiri::HTML(open(url))
-  end
-
-  def build_conditions_query conditions_equal, conditions_less_or_equal
-    all_conditions = conditions_equal.map{|condition, value| "#{condition} = #{value.inspect.gsub("\"","\'")}"} + \
-                     conditions_less_or_equal.map{|condition, value| "#{condition} <= #{value.inspect.gsub("\"","\'")}"}
-    query = all_conditions.join(' AND ') 
-
-    "(#{query})" 
-  end
 end
 
---
+++ lib/scraper.rb
@@ -0,0 +1,28 @@
+# TODO: make a factory
+class Scraper
+  # def get_houses_list
+  # end 
+
+  # def scrap_house
+  # end
+
+  def clean_field element 
+    # TODO: bad smell! esto no va aca!
+    element.inner_html.gsub(/&nbsp;/, '').downcase
+  end
+
+  def get_document_from_url base_url, params=nil
+    #TODO: tampoco va aca
+    url = URI.escape(@urls[base_url] + params)
+    Nokogiri::HTML(open(url))
+  end
+
+  def build_conditions_query conditions_equal, conditions_less_or_equal
+    all_conditions = conditions_equal.map{|condition, value| "#{condition} = #{value.inspect.gsub("\"","\'")}"} + \
+                     conditions_less_or_equal.map{|condition, value| "#{condition} <= #{value.inspect.gsub("\"","\'")}"}
+    query = all_conditions.join(' AND ') 
+
+    "(#{query})" 
+  end
+end
+
