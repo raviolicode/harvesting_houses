9623917db2a6a3fa62a58148e4cbbc4ec0da2eaa
Lucia Escanellas <raviolicode@gmail.com>
2011-03-27 16:55:20 -0300

    fixed method name on main

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require File.join(File.dirname(__FILE__), 'lib', 'bc_scraper')
 
 bc = BuscandoCasaScraper.new
-refs = bc.houses_reference_ids
+refs = bc.search_houses
 refs.each do |ref|
   puts '*-----------------------------------*'
   puts ref
